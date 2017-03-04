000e8ee9ef63ebac28a67a800b264bdc1b409f3b
Dana Nicole Llanes <dnllanes@icloud.com>
2017-03-04 10:27:06 +0800

    Update selection_sort.cpp

--- selection_sort.cpp
+++ selection_sort.cpp
@@ -1,24 +1,32 @@
 #include<iostream>
 using namespace std;
-
 int main(){
-	int A[]={30,40,20,50,10}, index,n,j;
-	for(int i=7;i>1;i--)
-		for (int i=0; i<7-1;i++){
-		int min=A[i], index =i;
-		for(int j=i+1; j<6;j++){
-			if(A[j]<min){
-				min=A[j];
-				index=j;
+	int A[]={30,40,20,50,10};
+	int n=5, y=1;
+	
+	cout<<"Array: ";
+	for(int e=0; e<5; e++)
+		cout<<A[e]<<" ";
+		cout<<endl<<endl;
+	
+	for(int i=0; i<n-1; i++){
+		int min=A[i], index=i;
+		for(int j=i+1; j<n; j++)
+			if (A[j] < min){
+				min = A[j];
+				index = j;
 			}
-		}
+
 		A[index]=A[i];
 		A[i]=min;
-	} 
-	cout<<"Sorted array:\n";
-		for (int i=1; i<7-1;i++)
-		cout<<A[i]<<""<<endl;
+		cout<<endl;
+		cout<<"Switches: "<<A[index]<<" and "<<A[i]<<endl;
 		
-	
-return 0;
+		cout<<"Pass no. "<<y++<<": ";
+		for (int i=0; i<5; i++){
+			cout<<A[i]<<" ";
+		}
+		cout<<endl;
+	}
+	return 0;
 }
